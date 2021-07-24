8cc21a93bc4116d30f85090459362a4f7f908070
Chris <chris@verida.io>
2021-07-24 21:57:28 +0930

    Add placeholder comments for next round of unit tests

--- packages/network-ts/test/database.tests.ts
+++ packages/network-ts/test/database.tests.ts
@@ -33,7 +33,7 @@ describe('Storage initialization tests', () => {
         ceramicUrl: CONFIG.CERAMIC_URL
     })
 
-    describe('Initialize user storage contexts', function() {
+    describe('Manage storage contexts for the authenticated user', function() {
         this.timeout(100000)
         
         it('can open a database with owner/owner permissions', async function() {
@@ -79,6 +79,31 @@ describe('Storage initialization tests', () => {
             assertIsValidDbResponse(assert, data)
             assert.ok(data[0].hello == 'world', 'First result has expected value')
         })
-        
+    })
+
+    describe('Access public storage contexts', function() {
+        this.timeout(100000)
+
+        // open an external context
+        // read from an external storage context with public read
+        // write to an external storage context with public write
+        // can't read from an external storage context with read=owner
+        // can't read from an external storage context with read=users
+        // can't write to an external storage context with write=owner
+        // can't write to an external storage context with write=users
+    })
+
+    describe('Access user storage contexts', function() {
+        this.timeout(100000)
+
+        // open an external context
+        // read from an external storage context with read=user where current user CAN read
+        // can't read from an external storage context with read=user where current user CANT read
+
+        // write to an external storage context with write=user where current user CAN write
+        // can't write to an external storage context with write=user where current user CANT write
+
+        // can't read from an external storage context with read=owner
+        // can't write to an external storage context with write=owner
     })
 })
