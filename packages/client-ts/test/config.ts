e7b178e69d037fe5b1186bbe5e49c8d6af0f8d5b
Chris <chris@verida.io>
2021-08-31 11:07:21 +0930

    Output encryption key in info() for encrypted database instances.

--- packages/client-ts/test/config.ts
+++ packages/client-ts/test/config.ts
@@ -3,10 +3,7 @@ export default {
     STORAGE_LINK_SCHEMA: 'kjzl6cwe1jw145l8jya7g6cuyluj17xlyc6t7p6iif12isbi1ppu5cuze4u3njc',
     CERAMIC_URL: 'https://ceramic-clay.3boxlabs.com',
     ETH_PRIVATE_KEY: '0x18d3b996ec98a9a536efdffbae40e5eaaf117765a587483c69195c9460165c8f',
-    DID: 'did:3:kjzl6cwe1jw1482rjifeq5ai96teeas0vm6hmcp9q5rmhk14th76f4d2tlnvy4d',
     ETH_PRIVATE_KEY_2: '0x28d3b996ec98a9a536efdffbae40f5eaaf117765a587483c69195c9460165c8f',
-    DID_2: 'did:3:kjzl6cwe1jw148dqa1a6bgxzrv6scw7ezw531vh02gp09jc2mcndcks15njf8pm',
     ETH_PRIVATE_KEY_3: '0xc8d3b996ec98a91536efdffbae40f5eaaf117765a587483c69195c9460165c8f',
-    DID_3: 'did:3:kjzl6cwe1jw14awervlhqdok3jnlqkd9qhm82azhol3n3qy4sg1og2mckonl5y2',
     CONTEXT_NAME: 'My Test Application'
 }
