f34826bfeaec321fd68b2a5a9aa5fdb7dda496b9
Chris <chris@verida.io>
2021-09-07 09:10:34 +0930

    Expose Messaging interface

--- packages/client-ts/src/index.ts
+++ packages/client-ts/src/index.ts
@@ -2,10 +2,12 @@ import Client from './client'
 import Network from './network'
 import AccountInterface from './account-interface'
 import Context from './context/context'
+import Messaging from './context/messaging'
 
 export {
     Client,
     Context,
     Network,
-    AccountInterface
+    AccountInterface,
+    Messaging
 }
