9875ce538cc34323d955873e321cdc56c86fdfd1
Chris <chris@verida.io>
2021-09-08 22:55:47 +0930

    Add untested NEAR support

---
+++ packages/3id-utils-node/src/chains/near.ts
@@ -0,0 +1,82 @@
+import { NearAuthProvider } from '@ceramicnetwork/blockchain-utils-linking'
+import { Manager } from '@3id/manager'
+import CeramicClient from '@ceramicnetwork/http-client'
+const nearAPI = require('near-api-js')
+const nearSeedPhrase = require('near-seed-phrase')
+const bs58 = require('bs58')
+const crypto = require('crypto')
+
+class NearProvider {
+
+    public wallet: any
+
+    constructor(mnemonic: string) {
+        const { secretKey, publicKey } = nearSeedPhrase.parseSeedPhrase(mnemonic)
+        this.wallet = {
+            mnemonic,
+            privateKey: secretKey,
+            publicKey: publicKey,
+            address: this.implicitAccountId(publicKey)
+        }
+    }
+
+    public async sign(message: string) {
+        const { KeyPair } = nearAPI
+        const keyPair = KeyPair.fromString(this.wallet.privateKey)
+        const messageBuffer = Buffer.from(message)
+        const hash = crypto.createHash('sha256').update(messageBuffer).digest()
+        const signature = keyPair.sign(hash)
+        return '0x' + Buffer.from(signature.signature).toString('hex')
+    }
+
+    private implicitAccountId(publicKey: string) {
+        return bs58.decode(publicKey.replace('ed25519:', '')).toString('hex');
+    }
+
+}
+
+const CHAIN_REF = '1' // ????
+
+export default class NearUtils {
+    
+    /**
+     * Create a new 3ID account from a near ??.
+     * Will automatically create if the 3ID doesn't exist.
+     * 
+     * @param privateKey Ethereum private key (hex) to create / get 3ID
+     * @param address Ethereum address (hex)
+     * 
+     * @returns DID instance
+     */
+    static async createAccount(seedPhrase: string, ceramic: CeramicClient): Promise<CeramicClient | undefined>  {
+        const manager = await NearUtils._getManager(seedPhrase, ceramic)
+        const did = await manager.createAccount()
+        return ceramic
+    }
+
+    // link an ethereum wallet to an existing 3ID
+    /**
+     * Link an existing Ethereum account to an existing 3ID
+     * 
+     * @param privateKey Ethereum private key (hex) to create / get 3ID
+     * @param did3id The existing 3ID to link to this existing Ethereum account
+     * 
+     * @returns DID instance
+     */
+     static async linkAccount(seedPhrase: string, did3id: string, ceramic: CeramicClient): Promise<CeramicClient> {
+        const manager = await NearUtils._getManager(seedPhrase, ceramic)
+        await manager.addAuthAndLink(did3id)
+        return ceramic
+    }
+
+    // @todo: Unlink an account
+
+    static async _getManager(seed: string, ceramic: CeramicClient): Promise<Manager> {
+        const provider = new NearProvider(seed)
+        const authProvider = new NearAuthProvider(provider, provider.wallet.address, CHAIN_REF)
+        
+        const manager = new Manager(authProvider, { ceramic })
+        return manager
+    }
+
+}
