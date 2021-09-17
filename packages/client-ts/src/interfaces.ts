4dfd155fcc71cea95e84f7f7439e9d95ce249904
tahpot <chris.were@gmail.com>
2021-09-17 15:48:41 +0930

    Feature/17 update code documentation (#45)
    * Improve README.md files
    * Add API documentation

--- packages/client-ts/src/interfaces.ts
+++ packages/client-ts/src/interfaces.ts
@@ -2,9 +2,31 @@ import { Interfaces } from '@verida/storage-link'
 import { Account } from '@verida/account'
 import BaseStorageEngine from './context/engines/base'
 
+export enum ClientEnvironment {
+    LOCAL = "local",
+    TESTNET = "testnet",
+    MAINNET = "mainnet"
+}
+
+/**
+ * Configuration for creating a new Client instance.
+ */
 export interface ClientConfig {
-    environment?: string
+    /**
+     * Environment to load by default.
+     * 
+     * Environment configuration can still be overriden by config items.
+     */
+    environment?: ClientEnvironment
+
+    /**
+     * URL of Ceramic node to use.
+     */
     ceramicUrl?: string
+
+    /**
+     * Specify custom schema paths (typicaly for local development).
+     */
     schemaPaths?: object
 }
 
@@ -19,14 +41,18 @@ export interface NetworkConnectionConfig {
     account: Account
 }
 
-// key = contextName
-// value = SecureStorageContextConfig
+/**
+ * key = contextName
+ * value = SecureStorageContextConfig
+ */
 export interface DIDContextConfigs {
     [key: string]: Interfaces.SecureContextConfig
 }
 
-// key = DID string
-// value = BaseStorageEngine
+/**
+ * key = DID string
+ * value = BaseStorageEngine
+ */
 export interface DatabaseEngines {
     [key: string]: BaseStorageEngine
 }
