206ddd1bc063dd037a85f3f29a7f8ccd205fbbca
Michael Chinweike <infinity_michael@yahoo.com>
2021-09-22 01:33:46 +0100

    feat: added correct redirect url for mobile device #46

--- packages/account-web-vault/src/auth-client.ts
+++ packages/account-web-vault/src/auth-client.ts
@@ -1,106 +1,122 @@
-import { AuthClientConfig, AuthResponse } from "./interfaces"
-import EncryptionUtils from "@verida/encryption-utils"
-import QrCode from 'qrcode-with-logos'
-const _ = require("lodash")
+import { AuthClientConfig, AuthResponse } from "./interfaces";
+import EncryptionUtils from "@verida/encryption-utils";
+import QrCode from "qrcode-with-logos";
+const _ = require("lodash");
 
 export default class AuthClient {
+  ws: any;
+  config: any;
+  symKeyBytes?: Uint8Array;
+  modal: any;
 
-    ws: any
-    config: any
-    symKeyBytes?: Uint8Array
-    modal: any
+  constructor(config: any, modal: any) {
+    this.config = _.merge(
+      {
+        schemeUri: "veridavault://login-request",
+        loginUri: "https://vault.verida.io/request/",
+        deeplinkId: "verida-auth-client-deeplink",
+        request: {},
+      },
+      config
+    );
+    this.modal = modal;
 
-    constructor(config: any, modal: any) {
-        this.config = _.merge({
-            schemeUri: 'veridavault://login-request',
-            loginUri: 'https://vault.verida.io/request/',
-            deeplinkId: 'verida-auth-client-deeplink',
-            request: {}
-        }, config)
-        this.modal = modal
+    const symKeyBytes = (this.symKeyBytes = EncryptionUtils.randomKey(32));
 
-        const symKeyBytes = this.symKeyBytes = EncryptionUtils.randomKey(32)
+    this.ws = new WebSocket(config.serverUri);
+    const client = this;
 
-        this.ws = new WebSocket(config.serverUri)
-        const client = this
+    this.ws.onmessage = function (event: MessageEvent) {
+      client.newMessage(event);
+    };
 
-        this.ws.onmessage = function (event: MessageEvent) {
-            client.newMessage(event)
+    config = this.config;
+    this.ws.onopen = function () {
+      const encryptedRequest = EncryptionUtils.symEncrypt(
+        JSON.stringify(config.request),
+        symKeyBytes
+      );
+      const payload = {
+        request: encryptedRequest,
+      };
+
+      client.ws.send(
+        JSON.stringify({
+          type: "generateJwt",
+          context: config.context,
+          payload,
+        })
+      );
+    };
+
+    this.ws.onerror = this.error;
+  }
+
+  newMessage(event: MessageEvent) {
+    const response = <AuthResponse>JSON.parse(event.data);
+
+    switch (response.type) {
+      case "auth-client-request":
+        const queryParams = this.generateQueryParams(response.message!);
+        const redirectUri = `${this.config.loginUri}${queryParams}`;
+        const schemeUri = `${this.config.schemeUri}${queryParams}`;
+        let qrcode = new QrCode({
+          canvas: document.getElementById(this.config.canvasId!) as any,
+          content: redirectUri,
+          width: 380,
+          image: document.getElementById("image") as any,
+          logo: {
+            src: "https://assets.verida.io/verida_logo_512x512.png",
+          },
+        });
+
+        try {
+          const isMobile = window.matchMedia(
+            "only screen and (max-width: 760px)"
+          ).matches;
+          if (isMobile) {
+            // On a mobile device, so attempting to auto-redirect to application
+            window.location.href = redirectUri;
+          }
+        } catch (err) {
+          console.log(err);
         }
 
-        config = this.config
-        this.ws.onopen = function () {
-            const encryptedRequest = EncryptionUtils.symEncrypt(JSON.stringify(config.request), symKeyBytes)
-            const payload = {
-                request: encryptedRequest
-            }
+        const deeplinkElm = document.getElementById(this.config.deeplinkId!);
 
-            client.ws.send(JSON.stringify({ type: 'generateJwt', context: config.context, payload }))
+        if (deeplinkElm) {
+          deeplinkElm.setAttribute("href", schemeUri);
         }
+        qrcode
+          .toCanvas()
+          .then(() => {})
+          .catch((error: any) => {
+            console.error("Error: ", { error });
+          });
+        return;
+      case "auth-client-response":
+        const key = this.symKeyBytes!;
+        const checkedValue: HTMLElement | any =
+          document.getElementById("verida-checked");
+        const decrypted = EncryptionUtils.symDecrypt(response.message, key);
 
-        this.ws.onerror = this.error
+        this.modal.style.display = "none";
+        this.config.callback(decrypted, checkedValue.checked);
+        return;
     }
 
-    newMessage(event: MessageEvent) {
-        const response = <AuthResponse>JSON.parse(event.data)
+    console.error(`Unknown message type: ${response.type}`, response);
+  }
 
-        switch (response.type) {
-            case 'auth-client-request':
-                const queryParams = this.generateQueryParams(response.message!)
-                const redirectUri = `${this.config.loginUri}${queryParams}`
-                const schemeUri = `${this.config.schemeUri}${queryParams}`
-                let qrcode = new QrCode({
-                    canvas: document.getElementById(this.config.canvasId!) as any,
-                    content: redirectUri,
-                    width: 380,
-                    image: document.getElementById("image") as any,
-                    logo: {
-                        src: "https://assets.verida.io/verida_logo_512x512.png"
-                    }
-                });
+  error(event: MessageEvent) {
+    console.error("WebSocket error: ", event);
+  }
 
-                try {
-                    const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-                    if (isMobile) {
-                        // On a mobile device, so attempting to auto-redirect to application
-                        window.location.href = schemeUri
-                    }
-                } catch (err) {
-                    console.log(err)
-                }
+  generateQueryParams(didJwt: string) {
+    const symKeyHex = "0x" + Buffer.from(this.symKeyBytes!).toString("hex");
 
-                const deeplinkElm = document.getElementById(this.config.deeplinkId!)
-
-                if (deeplinkElm) {
-                    deeplinkElm.setAttribute('href', schemeUri)
-                }
-                qrcode.toCanvas().then(() => {}).catch((error: any) => {
-                    console.error("Error: ", { error })
-                });
-                return
-            case 'auth-client-response':
-                const key = this.symKeyBytes!
-                const checkedValue: HTMLElement | any = document.getElementById('verida-checked');
-                const decrypted = EncryptionUtils.symDecrypt(response.message, key)
-
-                this.modal.style.display = 'none'
-                this.config.callback(decrypted, checkedValue.checked)
-                return
-        }
-
-        console.error(`Unknown message type: ${response.type}`, response)
-    }
-
-    error(event: MessageEvent) {
-        console.error("WebSocket error: ", event)
-    }
-
-    generateQueryParams(didJwt: string) {
-        const symKeyHex = "0x" + Buffer.from(this.symKeyBytes!).toString("hex")
-
-        // note: can't use `key` as a parameter as its a reserved word in react native
-        // instead use `_k` (key) and `_r` (request)
-        return `?_k=${symKeyHex}&_r=${didJwt}`
-    }
-
+    // note: can't use `key` as a parameter as its a reserved word in react native
+    // instead use `_k` (key) and `_r` (request)
+    return `?_k=${symKeyHex}&_r=${didJwt}`;
+  }
 }
