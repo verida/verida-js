d1c3fb551e76a3b0acf210c71bd336985f77d9d0
Chris <chris@verida.io>
2021-09-27 20:28:19 +0930

    Export context interfaces from client library

--- packages/client-ts/src/index.ts
+++ packages/client-ts/src/index.ts
@@ -2,10 +2,12 @@ import Client from './client'
 import Network from './network'
 import Context from './context/context'
 import Messaging from './context/messaging'
+import * as ContextInterfaces from './context/interfaces' 
 
 export {
     Client,
     Context,
     Network,
-    Messaging
+    Messaging,
+    ContextInterfaces
 }
