8b23623da2336a361b56ae3b5e1e4d079ab60d2f
Chris <chris@verida.io>
2021-10-27 21:12:28 +1030

    Fix did-document dependency issue. Update unit tests to use testnet DID server.

--- packages/client-ts/test/config.ts
+++ packages/client-ts/test/config.ts
@@ -2,7 +2,7 @@ import { EnvironmentType } from "@verida/account"
 
 export default {
     DID_SERVER_URL: undefined,// 'http://localhost:5001',
-    ENVIRONMENT: EnvironmentType.LOCAL,
+    ENVIRONMENT: EnvironmentType.TESTNET,
     VDA_PRIVATE_KEY: '0x78d3b996ec98a9a536efdffbae40e5eaaf117765a587483c69195c9460165c36',
     VDA_PRIVATE_KEY_2: '0x78d3b996ec98a9a536efdffbae40f5eaaf117765a587483c69195c9460165c36',
     VDA_PRIVATE_KEY_3: '0x78d3b996ec98a91536efdffbae40f5eaaf117765a587483c69195c9460165c36',
