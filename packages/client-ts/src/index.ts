1b1a4529fd557dafb0721eede4ebab8d33676e89
Chris <chris@verida.io>
2021-10-27 13:34:28 +1030

    [client-ts] Export environment types for easier config

--- packages/client-ts/src/index.ts
+++ packages/client-ts/src/index.ts
@@ -2,8 +2,8 @@ import Client from './client'
 import Network from './network'
 import Context from './context/context'
 import Messaging from './context/messaging'
-import * as ContextInterfaces from './context/interfaces' 
-import { ClientEnvironment } from './interfaces'
+import * as ContextInterfaces from './context/interfaces'
+import { EnvironmentType } from "@verida/account"
 
 export {
     Client,
@@ -11,5 +11,5 @@ export {
     Network,
     Messaging,
     ContextInterfaces,
-    ClientEnvironment
+    EnvironmentType
 }
