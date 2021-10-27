ac2838007c968217935b292651cb076354d60195
Chris <chris@verida.io>
2021-10-27 22:07:59 +1030

    Update unit test config config to use testnet servers

--- packages/client-ts/test/config.ts
+++ packages/client-ts/test/config.ts
@@ -7,16 +7,16 @@ export default {
     VDA_PRIVATE_KEY_2: '0x78d3b996ec98a9a536efdffbae40f5eaaf117765a587483c69195c9460165c36',
     VDA_PRIVATE_KEY_3: '0x78d3b996ec98a91536efdffbae40f5eaaf117765a587483c69195c9460165c36',
     CONTEXT_NAME: 'Verida Test: Test Application 1',
-    DATABASE_SERVER: 'http://localhost:5000/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
-    MESSAGE_SERVER: 'http://localhost:5000/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
+    DATABASE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
+    MESSAGE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
     DEFAULT_ENDPOINTS: {
         defaultDatabaseServer: {
             type: 'VeridaDatabase',
-            endpointUri: 'http://localhost:5000/'
+            endpointUri: 'https://db.testnet.verida.io:5002/'
         },
         defaultMessageServer: {
             type: 'VeridaMessage',
-            endpointUri: 'http://localhost:5000/'
+            endpointUri: 'https://db.testnet.verida.io:5002/'
         },
     }
 }
