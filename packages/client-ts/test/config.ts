69a3d4c9bd4bbf937dd7546a8d93a8404b60ff9a
Chris <chris@verida.io>
2021-11-08 21:14:36 +1030

    Create new private keys for testing to reset invalid database indexes that use deprecated schemas.

--- packages/client-ts/test/config.ts
+++ packages/client-ts/test/config.ts
@@ -3,9 +3,9 @@ import { EnvironmentType } from "@verida/account"
 export default {
     DID_SERVER_URL: undefined,// 'http://localhost:5001',
     ENVIRONMENT: EnvironmentType.TESTNET,
-    VDA_PRIVATE_KEY: '0x78d3b996ec98a9a536efdffbae40e5eaaf117765a587483c69195c9460165c36',
-    VDA_PRIVATE_KEY_2: '0x78d3b996ec98a9a536efdffbae40f5eaaf117765a587483c69195c9460165c36',
-    VDA_PRIVATE_KEY_3: '0x78d3b996ec98a91536efdffbae40f5eaaf117765a587483c69195c9460165c36',
+    VDA_PRIVATE_KEY:   '0x78d3b996ec98a9a536efdffbae40e5eaaf117765a587483c69195c9460165c37',
+    VDA_PRIVATE_KEY_2: '0x79d3b996ec98a9a536efdffbae40f5eaaf117765a587483c69195c9460165c37',
+    VDA_PRIVATE_KEY_3: '0x80d3b996ec98a91536efdffbae40f5eaaf117765a587483c69195c9460165c37',
     CONTEXT_NAME: 'Verida Test: Test Application 1',
     DATABASE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
     MESSAGE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
