7194b96d142825734ef11200d199dd6aa7beb2e4
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-02-10 16:05:26 +0100

    feat:added test for accessing data with different account #142

---
+++ packages/verifiable-credentials/test/config.ts
@@ -0,0 +1,50 @@
+import { AutoAccount } from '@verida/account-node';
+import { EnvironmentType, Network } from '@verida/client-ts';
+
+export const config = {
+    PRIVATE_KEY_1: "0x5dd84b6d500bcbc018cbc71b0407d694095755d91af42bd3442b2dfc96b1e0af",
+    PRIVATE_KEY_2: "0x80d3b996ec98a91536efdffbae40f5eaaf117765a587483c69195c9460165c37",
+    DID_1: 'did:vda:0xB3729982A2585544FD72c99CF3773a9c6baBD55c',
+    ADDRESS_1: '0xB3729982A2585544FD72c99CF3773a9c6baBD55c',
+    CREDENTAIL_DATA: {
+        id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+        alumniOf: 'Example University',
+        schema:
+            'https://common.schemas.verida.io/credential/public/default/v0.1.0/schema.json',
+    },
+    VERIDA_CONTEXT_NAME: "Verida: Credentials",
+    VERIDA_EXPECTED_DATABASE: 'credential_public_encrypted',
+    VERIDA_TESTNET_DEFAULT_SERVER: "https://db.testnet.verida.io:5002/",
+    VERIDA_URI: `verida://did:vda:0xB3729982A2585544FD72c99CF3773a9c6baBD55c/0x42406d644c8e884be0f76ee05539cfba03eda65baa2bda33ecd364ce4d18047c/credential_public_encrypted/6273bb80-596a-1331ec-ad0d-f3b4d6aec150?key=1f52db207c48b6c54f6692cb182c41bc3bd0805d0efdc9dcb25f15ade8639d5f`,
+    INVALID_VC_JWT: `eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSIsImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL2V4YW1wbGVzL3YxIl0sImlkIjoiIiwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCJdLCJpc3N1ZXIiOiJkaWQ6dmRhOjB4QjM3Mjk5ODJBMjU4NTU0NEZENzJjOTlDRjM3NzNhOWM2YmFCRDU1YyIsImlzc3VhbmNlRGF0ZSI6IjIwMjItMDItMTBUMDc6MDg6MTQuMDQwWiIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImlkIjoiZGlkOmV4YW1wbGU6ZWJmZWIxZjcxMmViYzZmMWMyNzZlMTJlYzIxIiwiYWx1bW5pT2YiOiJFeGFtcGxlIFVuaXZlcnNpdHkiLCJzY2hlbWEiOiJodHRwczovL2NvbW1vbi5zY2hlbWFzLnZlcmlkYS5pby9jcmVkZW50aWFsL3B1YmxpYy9kZWZhdWx0L3YwLjEuMC9zY2hlbWEuanNvbiJ9LCJjcmVkZW50aWFsU2NoZW1hIjp7ImlkIjoiaHR0cHM6Ly9jb21tb24uc2NoZW1hcy52ZXJpZGEuaW8vY3JlZGVudGlhbC9wdWJsaWMvZGVmYXVsdC92MC4xLjAvc2NoZW1hLmpzb24iLCJ0eXBlIjoiSnNvblNjaGVtYVZhbGlkYXRvcjIwMTgifX0sInN1YiI6ImRpZDp2ZGE6MHhCMzcyOTk4MkEyNTg1NTQ0RkQ3MmM5OUNGMzc3M2E5YzZiYUJENTVjIiwiaXNzIjoiZGlkOnZkYToweEIzNzI5OTgyQTI1ODU1NDRGRDcyYzk5Q0YzNzczYTljNmJhQkQ1NWMifQ.U2awX7ff-hPzUmMo4_VT0N-ORrQcGsnVTS5azw1iIH-6axMorvl30irBRCdWbn07TDM9ESFeFDLEXLguEXQ50w`
+
+}
+
+
+export const connect = async (privateKey: string) => {
+    const context = await Network.connect({
+        context: {
+            name: config.VERIDA_CONTEXT_NAME,
+        },
+        client: {
+            environment: EnvironmentType.TESTNET,
+        },
+        account: new AutoAccount(
+            {
+                defaultDatabaseServer: {
+                    type: 'VeridaDatabase',
+                    endpointUri: config.VERIDA_TESTNET_DEFAULT_SERVER,
+                },
+                defaultMessageServer: {
+                    type: 'VeridaMessage',
+                    endpointUri: config.VERIDA_TESTNET_DEFAULT_SERVER,
+                },
+            },
+            {
+                privateKey: privateKey,
+                environment: EnvironmentType.TESTNET,
+            }
+        ),
+    });
+    return context;
+};
