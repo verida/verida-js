01ebee428361b3b8fbb8de4c3c3beacc03c5d76c
Chris <chris@verida.io>
2022-02-14 15:32:50 +1030

    Commiting pending changes for Michael.

--- packages/verifiable-credentials/test/share-credentials.test.ts
+++ packages/verifiable-credentials/test/share-credentials.test.ts
@@ -23,30 +23,40 @@ describe('Share Credential tests', function () {
             credential = new Credentials(appContext)
         });
 
+        let createdUri = ''
+
         it('Issue an encrypted credential', async function () {
 
-            const item = await credential.createCredentialJWT(config.CREDENTIAL_DATA);
-            ;
+            const item = await credential.createCredentialJWT(config.SUBJECT_DID, config.CREDENTIAL_DATA);
+        
             const data = await shareCredential.issueEncryptedCredential(item);
+            createdUri = data.uri
 
             assert.ok(data.result.ok, 'Document was saved correctly');
 
             const expectedUri = Utils.buildVeridaUri(
-                config.DID_1,
+                config.ISSUER_DID,
                 config.VERIDA_CONTEXT_NAME,
                 config.VERIDA_EXPECTED_DATABASE,
                 data.result.id
             );
 
             const uriWithoutKey = data.uri.substring(0, expectedUri.length);
-            assert.equal(uriWithoutKey, expectedUri, 'URI is the expected value');
+            assert.equal(uriWithoutKey, expectedUri, 'URI is the expected value, without encryption key');
+
+            assert.equal(expectedUri, data.uri, 'URI is the expected value, with encryption key')
+
+            // @todo: Fetch the credential via data.uri, decode the credential and then ensure the vc.credentialSubject matches config.CREDENTIAL_DATA
         });
         it('Retrieve Credential data from URI using a different account', async function () {
+            // @todo: Fetch the credential via data.uri, decode the credential and then ensure the vc.credentialSubject matches config.CREDENTIAL_DATA
+            // BUT using config.PRIVATE_KEY_2
+
             const context = await connect(config.PRIVATE_KEY_2);
 
             const credential = new Credentials(context);
 
-            const jwt = await Utils.fetchVeridaUri(config.VALID_VERIDA_URI, context);
+            const jwt = await Utils.fetchVeridaUri(createdUri, context);
 
 
             const verifiedCredential: any = await credential.verifyCredential(jwt);
