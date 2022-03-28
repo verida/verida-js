d301bd91c28717788a6cd996b8746b9fc53f0b1e
Chris <chris@verida.io>
2022-03-28 18:40:23 +1030

    Create vault account request interface to make clear how logoUrl is configured.

--- packages/account-web-vault/src/interfaces.ts
+++ packages/account-web-vault/src/interfaces.ts
@@ -1,13 +1,16 @@
 import { Interfaces } from '@verida/storage-link'
 
+export interface VaultAccountRequest {
+    logoUrl?: string,        // Optional URL that will be displayed as part of the login process
+}
+
 export interface VaultAccountConfig {
     serverUri?: string,      // WSS URI
     loginUri?: string,       // Login URI (page where the user will be sent to login using the app; ie: vault.verida.io)
     canvasId?: string        // DOM id where the QR code canvas will be injected
     schemeUri?: string,
-    logoUrl?: string,
     deeplinkId?: string,
-    request?: object,        // Authorization request object that matches https://vault.schemas.verida.io/auth/loginRequest/latest/schema.json
+    request?: VaultAccountRequest,
     callback?(response: AuthResponse): void        // callback function (called when auth response received)
     callbackRejected?(): void   // callback function (called when user rejects / cancels the login by closing the modal)
 }
@@ -18,9 +21,8 @@ export interface AuthClientConfig {
     loginUri?: string,       // Login URI (page where the user will be sent to login using the app; ie: vault.verida.io)
     canvasId?: string        // DOM id where the QR code canvas will be injected
     schemeUri?: string,
-    logoUrl?: string,
     deeplinkId?: string,
-    request?: object,        // Authorization request object that matches https://vault.schemas.verida.io/auth/loginRequest/latest/schema.json
+    request?: VaultAccountRequest,
     callback(response: AuthResponse): void        // callback function (called when auth response received)
     callbackRejected?(): void   // callback function (called when user rejects / cancels the login by closing the modal)
 }
