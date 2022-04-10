379653623ce9f5b820ce41c48169770fb7a88472
Chris <chris@verida.io>
2022-04-11 08:22:11 +0930

    Don't expose utils

--- packages/verifiable-credentials/src/index.ts
+++ packages/verifiable-credentials/src/index.ts
@@ -1,5 +1,4 @@
 import Credentials from './credentials';
 import SharingCredential from './sharing-credential';
-import * as Utils from './utils'
 
-export { Credentials, SharingCredential, Utils };
+export { Credentials, SharingCredential };
