f58a2c67389023f10fc08b49331c9be6ed0fcb9a
Nick Lothian <nick@verida.io>
2022-05-23 08:14:55 +0930

    exporting client-ts API as top level exports

--- packages/client-ts/src/index.ts
+++ packages/client-ts/src/index.ts
@@ -2,6 +2,12 @@ import Client from './client'
 import Network from './network'
 import Context from './context/context'
 import Messaging from './context/messaging'
+import Datastore from './context/datastore'
+import Database from './context/database'
+import Notification from './context/notification'
+import { Profile } from './context/profiles/profile'
+import * as DbRegistry from './context/db-registry'
+import Schema from './context/schema'
 import * as ContextInterfaces from './context/interfaces'
 import { EnvironmentType } from "@verida/account"
 import * as Utils from './utils'
@@ -11,6 +17,12 @@ export {
     Context,
     Network,
     Messaging,
+    Database,
+    Datastore,
+    Notification,
+    Profile,
+    DbRegistry,
+    Schema,
     ContextInterfaces,
     EnvironmentType,
     Utils
