3064c60edd4af202caa44c3460505390039d3e6c
Chris <chris@verida.io>
2022-06-15 19:57:00 -0400

    Example code showing the core changes needed to start using decentralized DID registry.

--- packages/did-client/src/did-client.ts
+++ packages/did-client/src/did-client.ts
@@ -2,17 +2,32 @@ import Axios from 'axios'
 import { DIDDocument, Interfaces } from "@verida/did-document"
 import Wallet from "./wallet"
 
-export default class DIDClient {
+// @todo: Link this in package.json
+import { VeridaContractInstance } from '@verida/web3'
 
-    private endpointUrl: string
+const CONTRACT_ADDRESSES: any = {
+    'testnet': '0x2862BC860f55D389bFBd1A37477651bc1642A20B'
+}
 
+const didRegistryABI = require('./VDADIDRegistry.abi.json')
+
+export interface DIDClientConfig {
+    environment: string
+    connectionType: 'gasless' | 'web3',
+    headers?: object
+}
+
+export class DIDClient {
+
+    private config: DIDClientConfig
+
+    private didRegistry?: VeridaContractInstance
     private privateKey?: Uint8Array
     private did?: string
     private wallet?: Wallet
 
-    constructor(endpointUrl: string) {
-        // Strip any trailing slash
-        this.endpointUrl = endpointUrl.replace(/\/$/,'')
+    constructor(config: DIDClientConfig) {
+        this.config = config
     }
 
     /**
@@ -26,6 +41,26 @@ export default class DIDClient {
         this.wallet = new Wallet(privateKey)
         this.privateKey = this.wallet.privateKeyBuffer
         this.did = this.wallet.did
+
+        switch (this.config.connectionType) {
+            case 'gasless':
+                this.didRegistry = VeridaContractInstance(
+                    this.config.connectionType,
+                    {
+                        veridaKey: privateKey, // is this correct? might need to be this.privateKey converted to string?
+                        abi: didRegistryABI,
+                        address: CONTRACT_ADDRESSES[this.config.environment]
+                        // @todo: Assuming `headers` added to Web3Config
+                        //headers: this.config.headers ? this.config.headers : {}
+                    }
+                )
+                break
+            case 'web3':
+                // @todo: configure as appropriate
+                break
+            default:
+                throw new Error("Unknown blockchain connection type specified")
+        }
     }
 
     public getDid(): string | undefined {
@@ -43,13 +78,15 @@ export default class DIDClient {
      * @returns 
      */
     public async save(document: DIDDocument): Promise<boolean> {
-        if (!this.privateKey) {
-            throw new Error("Unable to save DIDDocument. No private key.")
+        if (!this.didRegistry) {
+            throw new Error("Unable to save DIDDocument. No account connected.")
         }
 
-        document.signProof(this.privateKey)
+        document.signProof(this.privateKey!)
 
-        try {
+        // @todo: use this.didRegistry to update the DID document
+
+        /*try {
             const response = await Axios.post(`${this.endpointUrl}/commit`, {
                 params: {
                     document: document.export()
@@ -63,11 +100,11 @@ export default class DIDClient {
             }
 
             throw err
-        }
+        }*/
     }
 
     public async get(did: string): Promise<DIDDocument | undefined> {
-        try {
+        /*try {
             const response: any = await Axios.get(`${this.endpointUrl}/load?did=${did}`);
             const documentData: Interfaces.DIDDocumentStruct = response.data.data.document
             const doc = new DIDDocument(documentData)
@@ -84,7 +121,9 @@ export default class DIDClient {
             }
 
             throw err
-        }
+        }*/
+
+        // @todo: use this.didRegistry to get the DID document
     }
 
 }
--- packages/did-client/src/index.ts
+++ packages/did-client/src/index.ts
@@ -1,7 +1,8 @@
-import DIDClient from "./did-client"
+import { DIDClient, DIDClientConfig } from "./did-client"
 import Wallet from "./wallet"
 
 export {
     DIDClient,
+    DIDClientConfig,
     Wallet
 }
