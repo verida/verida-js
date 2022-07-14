43befb59df5974af511f8f7c4d2ffd7f76a3fbbb
Alex <trust2ray@gmail.com>
2022-07-14 10:16:27 +0900

    Minor updates for PR review

--- packages/vda-web3-client/src/helpers.ts
+++ packages/vda-web3-client/src/helpers.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { BigNumber } from '@ethersproject/bignumber'
 
-// DID-Registry-Contract address on BSC testnet
 export const DEFAULT_REGISTRY_ADDRESS = '0x2862BC860f55D389bFBd1A37477651bc1642A20B'
 export const DEFAULT_JSON_RPC = 'http://127.0.0.1:8545/'
 
