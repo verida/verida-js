2f6da6a6183409dde033780df6ff3ca10f6ad433
Alex <trust2ray@gmail.com>
2022-08-05 15:09:47 +0900

    Test codes updated :
    Test done on Polygon mainnet

--- packages/did-client/test/polygon.test.ts.ts
+++ packages/did-client/test/polygon.test.ts.ts
@@ -45,7 +45,7 @@ describe('DID Document tests', () => {
             // vda-did resolver - only
             identifier : identity, // DID
             // chainName? : string, 
-            chainId : '0x61',
+            chainId : '0x89',
         
             // common to vda-did resolver & vda-did self transaction config
             provider,
@@ -64,21 +64,14 @@ describe('DID Document tests', () => {
         console.log('DIDClient created successfully')
     })
 
-    /*
-    it('Creating DID-Client',async () => {
-        const document = await didClient.getDocument()
-
-        console.log('TAG', document)
-    })
-    */
-
-    it('add SignKey test',async () => {
+    it ('Create a DIDDocument with one transaction',async () => {
         const document = await didClient.getDocument()
         console.log('******** Original DIDDocument**********')
-        console.log(document?.verificationMethod)
+        console.log(document)
 
-        const context = '0x888888eb5c3f53d8506e7085dfbb0ef333c5f7d0769bcaf4ca2dc0ca46d00011'
-        const pubKey = '0x999999b792710e80b7605fe4ac680eb7f070ffadcca31aeb0312df80f7300011'
+        // Add signkey
+        let context = '0x888888eb5c3f53d8506e7085dfbb0ef333c5f7d0769bcaf4ca2dc0ca46d00001'
+        let pubKey = '0x999999b792710e80b7605fe4ac680eb7f070ffadcca31aeb0312df80f7300001'
         document!.verificationMethod!.push({
             id:`${didClient.getDid()}?context=${context}`,
             type: 'EcdsaSecp256k1VerificationKey2019',
@@ -86,17 +79,78 @@ describe('DID Document tests', () => {
             publicKeyHex: `${pubKey}`
         })
 
-        console.log('Save document')
+        // Add AsymKey
+        context = '0x888888eb5c3f53d8506e7085dfbb0ef333c5f7d0769bcaf4ca2dc0ca46d00002'
+        pubKey = '0x999999b792710e80b7605fe4ac680eb7f070ffadcca31aeb0312df80f7300002'
+        let id = `${didClient.getDid()}?context=${context}`
+        document!.verificationMethod!.push({
+            id,
+            type: 'X25519KeyAgreementKey2019',
+            controller: `${didClient.getDid()}`,
+            publicKeyHex: `${pubKey}`
+        })
+
+        if (!(document!.keyAgreement)) {
+            document!.keyAgreement = []
+        }
+        document!.keyAgreement.push(id)
+
+        // Add service
+        if (!(document!.service)) {
+            document!.service = []
+        }
+
+        context = '0x777777eb5c3f53d8506e7085dfbb0ef333c5f7d0769bcaf4ca2dc0ca46d00002'
+        id = `${didClient.getDid()}?context=${context}&type=message`
+        document!.service!.push({
+            id,
+            type: 'VeridaMessage',
+            serviceEndpoint: 'https://db.testnet.verida.io:5002'
+        })
+
+        // Save all changes
+        await didClient.saveDocument(document!)
+
+        // console.log("Reloading document to check updates")
+
+        // LoadDocument again
+        const newDoc = await didClient.reloadDIDDocument()
+
+        console.log('********* Updtated DIDDocument**********')
+        console.log(newDoc)
+    })
+
+    /*
+    it('Creating DID-Client',async () => {
+        const document = await didClient.getDocument()
+
+        console.log('TAG', document)
+    })
+
+    it('add SignKey test',async () => {
+        const document = await didClient.getDocument()
+        console.log('******** Original DIDDocument**********')
+        console.log(document?.verificationMethod)
+
+        const context = '0x888888eb5c3f53d8506e7085dfbb0ef333c5f7d0769bcaf4ca2dc0ca46d00001'
+        const pubKey = '0x999999b792710e80b7605fe4ac680eb7f070ffadcca31aeb0312df80f7300001'
+        document!.verificationMethod!.push({
+            id:`${didClient.getDid()}?context=${context}`,
+            type: 'EcdsaSecp256k1VerificationKey2019',
+            controller: `${didClient.getDid()}`,
+            publicKeyHex: `${pubKey}`
+        })
+
         // keyPurpose of this attribute will be 'veriKey'
         await didClient.saveDocument(document!)
 
-        console.log('reload document')
         // LoadDocument again
         const newDoc = await didClient.reloadDIDDocument()
 
         console.log('********* Updtated DIDDocument**********')
         console.log(newDoc?.verificationMethod)
     })
+    */
 
     /*
     it('add AsymKey test', async () => {
