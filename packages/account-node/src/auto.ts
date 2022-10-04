dd38f0eacebbd0f519c40f0be72338467d80f62c
Chris <chris@verida.io>
2022-10-05 09:55:57 +1030

    Get account node tests working

--- packages/account-node/src/auto.ts
+++ packages/account-node/src/auto.ts
@@ -6,13 +6,14 @@ import { NodeAccountConfig } from './interfaces'
 import { DIDClient, Wallet } from '@verida/did-client'
 import EncryptionUtils from "@verida/encryption-utils"
 import { Interfaces as DIDDocumentInterfaces } from "@verida/did-document"
+import { Wallet as EthersWallet } from "ethers"
+import { JsonRpcProvider } from '@ethersproject/providers'
 
 /**
  * An Authenticator that automatically signs everything
  */
 export default class AutoAccount extends Account {
 
-    private privateKey: string
     private didClient: DIDClient
 
     private wallet: Wallet
@@ -23,11 +24,25 @@ export default class AutoAccount extends Account {
         this.accountConfig = accountConfig
         this.wallet = new Wallet(autoConfig.privateKey)
 
-        const didServerUrl = autoConfig.didServerUrl ? autoConfig.didServerUrl : Config.environments[autoConfig.environment].didServerUrl
-        this.didClient = new DIDClient(didServerUrl)
-        
-        this.privateKey = this.wallet.privateKey
-        this.didClient.authenticate(this.privateKey)
+        this.didClient = new DIDClient({
+            ...autoConfig.didClientConfig,
+            network: <'testnet' | 'mainnet'> autoConfig.environment
+        })
+
+        if (autoConfig.didClientConfig.callType == 'web3') {
+            const provider = new JsonRpcProvider(autoConfig.didClientConfig.rpcUrl)
+            const txSigner = new EthersWallet(autoConfig.didClientConfig.networkPrivateKey, provider)
+            // @ts-ignore Why doesn't pickup the interfaces?
+            autoConfig.didClientConfig.web3Config.provider = provider
+            // @ts-ignore Why doesn't pickup the interfaces?
+            autoConfig.didClientConfig.web3Config.signer = txSigner
+        }
+
+        this.didClient.authenticate(
+            autoConfig.didClientConfig.networkPrivateKey,
+            autoConfig.didClientConfig.callType,
+            autoConfig.didClientConfig.web3Config
+        )
     }
 
     public async keyring(contextName: string): Promise<Keyring> {
---
+++ packages/account-node/test/config.ts
@@ -0,0 +1,6 @@
+
+const CONFIG = {
+    networkPrivateKey: '',           // network private key for testing
+}
+
+export default CONFIG
