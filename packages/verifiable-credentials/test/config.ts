12502860f06f356a4223d59d8cc564c2c944829e
Chris <chris@verida.io>
2022-10-14 14:01:25 +1030

    Update share credential tests to work with decentralized did client.

--- packages/verifiable-credentials/test/config.ts
+++ packages/verifiable-credentials/test/config.ts
@@ -36,8 +36,8 @@ export default {
     VERIDA_CONTEXT_NAME: "Verida: Credentials",
     VERIDA_EXPECTED_DATABASE: 'credential_public_encrypted',
     VERIDA_TESTNET_DEFAULT_SERVER: "https://db.testnet.verida.io:5002/",
-    INVALID_VERIDA_URI: `verida://did:vda:0xB3729982A2585544FD72c99CF3773a9c6baBD55c/0x42406d644c8e884be0f76ee05539cfba03eda65baa2bda33ecd364ce4d18047c/credential_public_encrypted/6273bb80-596a-1331ec-ad0d-f3b4d6aec150?key=1f52db207c48b6c54f6692cb182c41bc3bd0805d0efdc9dcb25f15ade8639d5f`,
-    EXISTING_CREDENTIAL_URI: `verida://did:vda:0xB3729982A2585544FD72c99CF3773a9c6baBD55c/GpNDePvWU4RQud9jaZJiNwFyAS/credential_public_encrypted/6ccd0700-939c-11ec-9ac4-371a75288755?key=b572983d88023c3adda7a7f3066c817f2f1b404a7cedf4bae3b0dd09b9960c79`,
+    INVALID_VERIDA_URI: `verida://did:vda:testnet:0xB3729982A2585544FD72c99CF3773a9c6baBD55c/0x42406d644c8e884be0f76ee05539cfba03eda65baa2bda33ecd364ce4d18047c/credential_public_encrypted/6273bb80-596a-1331ec-ad0d-f3b4d6aec150?key=1f52db207c48b6c54f6692cb182c41bc3bd0805d0efdc9dcb25f15ade8639d5f`,
+    EXISTING_CREDENTIAL_URI: `verida://did:vda:testnet:0x0eB0FeD3Fc4f29303fF462fA789E417936E21eee/GpNDePvWU4RQud9jaZJiNwFyAS/credential_public_encrypted/28916130-4b70-11ed-b782-b5bccbc8755f?key=24104769d8815432029f533bc7dd0872d930955d23f21c27537a9e5297f5baeb`,
     RAW_CREDENTIAL_DATA: {
         name: 'Vitalik Buterin',
         firstName: 'Vitalik',
