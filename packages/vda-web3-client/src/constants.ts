8bb99efbbdc3e93a8d8935d5c0e79382b1fd9c8b
Alex J <trust2ray@gmail.com>
2022-11-16 18:02:59 +0900

    Feature/253 vda web3 client vip2.0 (#254)
    * VDA-Web3-Client : DIDRegistry Test working
    
    Next:
    	NameRegistry test code update
    
    * Feature/63 Update test codes
    
    Updated:
    	Test codes for DIDRegistry & NameRegistry
    	Whitelist of contracts in src/constants.ts
    
    * Minor update

--- packages/vda-web3-client/src/constants.ts
+++ packages/vda-web3-client/src/constants.ts
@@ -2,7 +2,8 @@
 export const veridaContractWhiteList = [
   '0x5c5CA3376b2C82f0322DB9dEE0504D2565080865'.toLowerCase(), //BSCTest - NameRegistry
   '0x1a0A67467DB853486ae328cFdd2d7D51FaefC5E7'.toLowerCase(), //BSCTest - DIDRegistry
-  '0x0D10C68F52326C47Dfc3FDBFDCCb37e3b8C852Cb'.toLowerCase(), //Mumbai - DIDRegistry
+  '0x666F81e59082ccD7C8737f84e1C232a982043140'.toLowerCase(), //Mumbai - NameRegistry
+  '0x322F0273D7f6eCd9EeBc6C800a6777d1b3EEB697'.toLowerCase(), //Mumbai - DIDRegistry
 ];
 
 export const DEFAULT_JSON_RPC = 'http://127.0.0.1:8545/'
