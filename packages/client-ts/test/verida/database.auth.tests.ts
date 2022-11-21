75697c85c5549b23bdc76573bde0840a8e611578
Chris <chris@verida.io>
2022-11-21 20:01:30 +1030

    Auth tests working

--- packages/client-ts/test/verida/database.auth.tests.ts
+++ packages/client-ts/test/verida/database.auth.tests.ts
@@ -21,18 +21,18 @@ describe('Verida auth tests', () => {
 
     const account = new AutoAccount(CONFIG.DEFAULT_ENDPOINTS, {
         privateKey: CONFIG.VDA_PRIVATE_KEY,
-        didServerUrl: CONFIG.DID_SERVER_URL,
-        environment: CONFIG.ENVIRONMENT
+        environment: CONFIG.ENVIRONMENT,
+        didClientConfig: CONFIG.DID_CLIENT_CONFIG
     })
 
     const invalidAccount = new AutoAccount(CONFIG.INVALID_ENDPOINTS, {
         privateKey: CONFIG.VDA_PRIVATE_KEY,
-        didServerUrl: CONFIG.DID_SERVER_URL,
-        environment: CONFIG.ENVIRONMENT
+        environment: CONFIG.ENVIRONMENT,
+        didClientConfig: CONFIG.DID_CLIENT_CONFIG
     })
 
     const invalidNetwork = new Client({
-        didServerUrl: CONFIG.DID_SERVER_URL,
+        didClientConfig: CONFIG.DID_CLIENT_CONFIG,
         environment: CONFIG.ENVIRONMENT
     })
 
@@ -61,33 +61,39 @@ describe('Verida auth tests', () => {
     })
     
     describe('Handle token expiry', function() {
-        this.timeout(2000)
+        this.timeout(10000)
 
         it('can handle accessToken expiry for an encrypted database', async function() {
             // Create a working connection
             const network = new Client({
-                didServerUrl: CONFIG.DID_SERVER_URL,
+                didClientConfig: CONFIG.DID_CLIENT_CONFIG,
                 environment: CONFIG.ENVIRONMENT
             })
         
             const network2 = new Client({
-                didServerUrl: CONFIG.DID_SERVER_URL,
+                didClientConfig: CONFIG.DID_CLIENT_CONFIG,
                 environment: CONFIG.ENVIRONMENT
             })
 
             await network.connect(account)
             const context = await network.openContext(VALID_CONTEXT, true)
 
+            const db1 = await context!.openDatabase(DB_NAME_OWNER, {
+                saveDatabase: false
+            })
+
             // 1/ get the context auth
-            const authContext = await context.getAuthContext()
+            const did = await account.did()
+            // @ts-ignore
+            const authContext = context.databaseEngines[did].endpoints['http://localhost:5000/'].auth
 
             // Manually set to an invalid access token so the refreshToken is needed to re-authenticate
             authContext.accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVIUTlLTFprbE9UVERjaU1TOXFOY1Nzb0hBUUJSbUxDbEQ3d2pDUnBsSXNSUE45emZMdHBSM0JlM3d0cXRVVHlFakVPcVhpS2NZeG5zZHBWWTlWRWE3TmUwNUtsUElPUTQxSlpJQktHWW1NYjE2ajRkWkZxVThlek1VZFR1SnRFYmNKUlY0M09JWXE4OTF0ZXY0TDhKY2xPdzBZV1BweTFoMmpKTHkzblNJNVZsZ3RaVG1HVmI5cXlDODRrVnd1NGsySzVvN1VuOFkyUmNWbGpEa2lZRXpaanBxbzlkc0l4Mkh0UGNQcE9TVDhVM05WU29TWjFibzNBdU5CRjkxbE4iLCJkaWQiOiJkaWQ6dmRhOjB4ZTcwZTYyODQyNzg4MGFiMDFiZTU1YTM0OWYxY2VmMDQ3OWIyMWJmOCIsInN1YiI6InZmNWYxOWYyYzcyYTFmNDllYjcwMDIxNWMwNGVhMGM4NjVmMjc4ZTA4MGNkMDU5Njc4NjVhMmE5MTA0YWUzNjUzIiwiY29udGV4dE5hbWUiOiJWZXJpZGEgVGVzdGluZzogQXV0aGVudGljYXRpb24iLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjYxNDE5ODQ2LCJleHAiOjE2NjE0MTk5MDZ9.tHugqMHM_udE-eV0u2_2oCjzDSiljfK8DVieK-GcXcU'
-            
+
             // 2/ initialise a new testing account that has a single context with an invalid access token
             const account2 = new AuthContextAccount(CONFIG.DEFAULT_ENDPOINTS, {
                 privateKey: CONFIG.VDA_PRIVATE_KEY,
-                didServerUrl: CONFIG.DID_SERVER_URL,
+                didClientConfig: CONFIG.DID_CLIENT_CONFIG,
                 environment: CONFIG.ENVIRONMENT
             },
             VALID_CONTEXT,
@@ -111,20 +117,23 @@ describe('Verida auth tests', () => {
         it('can handle refreshToken expiry for an encrypted database', async function() {
             // Create a working connection
             const network = new Client({
-                didServerUrl: CONFIG.DID_SERVER_URL,
+                didClientConfig: CONFIG.DID_CLIENT_CONFIG,
                 environment: CONFIG.ENVIRONMENT
             })
         
             const network2 = new Client({
-                didServerUrl: CONFIG.DID_SERVER_URL,
+                didClientConfig: CONFIG.DID_CLIENT_CONFIG,
                 environment: CONFIG.ENVIRONMENT
             })
             
             await network.connect(account)
             const context = await network.openContext(VALID_CONTEXT, true)
+            await context?.openDatabase(DB_NAME_OWNER)
 
             // 1/ get the context auth
-            const authContext = await context.getAuthContext()
+            const did = await account.did()
+            // @ts-ignore
+            const authContext = context.databaseEngines[did].endpoints['http://localhost:5000/'].auth
 
             // Manually set to an invalid access token so the refreshToken is needed to re-authenticate
             authContext.accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVIUTlLTFprbE9UVERjaU1TOXFOY1Nzb0hBUUJSbUxDbEQ3d2pDUnBsSXNSUE45emZMdHBSM0JlM3d0cXRVVHlFakVPcVhpS2NZeG5zZHBWWTlWRWE3TmUwNUtsUElPUTQxSlpJQktHWW1NYjE2ajRkWkZxVThlek1VZFR1SnRFYmNKUlY0M09JWXE4OTF0ZXY0TDhKY2xPdzBZV1BweTFoMmpKTHkzblNJNVZsZ3RaVG1HVmI5cXlDODRrVnd1NGsySzVvN1VuOFkyUmNWbGpEa2lZRXpaanBxbzlkc0l4Mkh0UGNQcE9TVDhVM05WU29TWjFibzNBdU5CRjkxbE4iLCJkaWQiOiJkaWQ6dmRhOjB4ZTcwZTYyODQyNzg4MGFiMDFiZTU1YTM0OWYxY2VmMDQ3OWIyMWJmOCIsInN1YiI6InZmNWYxOWYyYzcyYTFmNDllYjcwMDIxNWMwNGVhMGM4NjVmMjc4ZTA4MGNkMDU5Njc4NjVhMmE5MTA0YWUzNjUzIiwiY29udGV4dE5hbWUiOiJWZXJpZGEgVGVzdGluZzogQXV0aGVudGljYXRpb24iLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjYxNDE5ODQ2LCJleHAiOjE2NjE0MTk5MDZ9.tHugqMHM_udE-eV0u2_2oCjzDSiljfK8DVieK-GcXcU'
@@ -133,7 +142,7 @@ describe('Verida auth tests', () => {
             // 2/ initialise a new testing account that has a single context with an invalid access and refresh token
             const account2 = new AuthContextAccount(CONFIG.DEFAULT_ENDPOINTS, {
                 privateKey: CONFIG.VDA_PRIVATE_KEY,
-                didServerUrl: CONFIG.DID_SERVER_URL,
+                didClientConfig: CONFIG.DID_CLIENT_CONFIG,
                 environment: CONFIG.ENVIRONMENT
             },
             VALID_CONTEXT,
