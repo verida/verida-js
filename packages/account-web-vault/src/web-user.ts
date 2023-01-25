0dc2d1c015c96d1aabbe5b0ae60aa1ff5c48ed9e
Chris <chris@verida.io>
2023-01-25 11:51:57 +1030

    Fix static properties that should be optional

--- packages/account-web-vault/src/web-user.ts
+++ packages/account-web-vault/src/web-user.ts
@@ -1,7 +1,7 @@
 import { Client, Network, Context } from '@verida/client-ts';
 import { VaultAccount, hasSession } from './index';
 import { EventEmitter } from 'events'
-import { IProfile, ClientConfig, ContextConfig, NetworkConnectionConfig, MessageSendConfig, IDatastore, IDatabase, DatabaseOpenConfig, DatastoreOpenConfig, AccountVaultConfig } from '@verida/types';
+import { IProfile, ClientConfig, ContextConfig, MessageSendConfig, IDatastore, IDatabase, DatabaseOpenConfig, DatastoreOpenConfig, AccountVaultConfig } from '@verida/types';
 
 const VeridaEvents = new EventEmitter()
 
@@ -53,8 +53,8 @@ export class WebUser {
     private static profile?: WebUserProfile
     private static did?: string
 
-    private static connecting: Promise<boolean>
-    private static profileConnection: IProfile
+    private static connecting?: Promise<boolean>
+    private static profileConnection?: IProfile
 
     public static configure(config: WebUserConfig): void {
         WebUser.config = config
@@ -197,7 +197,7 @@ export class WebUser {
             WebUser.account = undefined
             WebUser.profile = undefined
             WebUser.did = undefined
-            WebUser.connecting = new Promise((resolve) => { resolve(false) })
+            WebUser.connecting = undefined
 
             if (this.config.debug) {
                 console.log(`Account disconnected`)
