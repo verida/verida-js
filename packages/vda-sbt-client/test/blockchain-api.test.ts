a57c1b100b1064c9b6f43637ada9c4fdffeea754
Chris <chris@verida.io>
2023-03-06 19:00:08 +1030

    Update unit test to show the bug

--- packages/vda-sbt-client/test/blockchain-api.test.ts
+++ packages/vda-sbt-client/test/blockchain-api.test.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import { dids, getBlockchainAPIConfiguration } from "./utils"
+import { dids, getBlockchainAPIConfiguration, getNetwork, CONTEXT_NAME, RECIPIENT_WALLET } from "./utils"
 import { VeridaSBTClient } from "../src/index"
 import { ethers, Wallet } from "ethers";
 import EncryptionUtils from "@verida/encryption-utils";
@@ -21,7 +21,6 @@ const createBlockchainAPI = (did: any) => {
 
 describe('vda-sbt-client blockchain api', () => {
     let blockchainApi : VeridaSBTClient
-    
 
     before(() => {
         blockchainApi = createBlockchainAPI(did);
@@ -88,55 +87,50 @@ describe('vda-sbt-client blockchain api', () => {
         })
     })
 
-    describe("SBT claim & burn", () => {
-        /**
-         * `signedData` of the SBTInfo struct should be signed by `sbtSigner`.
-         * `signedProof` of the SBTInfo struct should be signed by `trustedSigner`.
-         */
-        const trustedSigner = dids[2]
-        const sbtSigner = dids[3]
-
-        // Claim a sbt to the "did" for burning test
-        const claimer = did.address
+    describe("SBT claim & burn", function() {
+        this.timeout(60*1000)
 
         // SBT info
-        const sbtType = "twitter"
-        const sbtId = "testId-3" //+ Wallet.createRandom().address
+        const sbtType = "twitter-test"
+        const uniqueId = "123"
         const sbtURI = "https://gateway.pinata.cloud/ipfs/QmVrTkbrzNHRhmsh88XnwJo5gBu8WqQMFTkVB4KoVLxSEY/3.json"
 
-        it("Claim a SBT successfully",async () => {
+        it.only("Claim a SBT successfully",async () => {
+            const trustedSigner = dids[2]
+            const trustedSignerNetworkInfo = await getNetwork(trustedSigner.privateKey)
+            const trustedDid = await trustedSignerNetworkInfo.account.did()
+            const trustedSignerDIDDocument = await trustedSignerNetworkInfo.account.getDidClient().get(trustedDid)
+
+            const sbtOwner = did
+            const sbtOwnerNetworkInfo = await getNetwork(sbtOwner.privateKey)
+            const sbtOwnerDid = await sbtOwnerNetworkInfo.account.did()
+
+            console.log(trustedDid, sbtOwnerDid)
+
             // Should check if the trustedSigner is registered to the contract
             const signers = await blockchainApi.getTrustedSignerAddresses()
             assert.ok(signers.includes(trustedSigner.address))
 
             // Create SBT Info
-            /// generate signedData of SBTInfo
-            let privateKeyArray = new Uint8Array(
-                Buffer.from(sbtSigner.privateKey.slice(2), "hex")
-            );
             const signedDataMsg = ethers.utils.solidityPack(
                 ['string','address'],
-                [`${sbtType}-${sbtId}-`, did.address]
+                [`${sbtType}-${uniqueId}`, sbtOwner.address]
             )
-            const signedData = EncryptionUtils.signData(signedDataMsg, privateKeyArray)
-
-            /// generate signedProof of SBTInfo
-            const signedProofMsg = `${trustedSigner.address}${sbtSigner.address}`.toLowerCase()
-            privateKeyArray = new Uint8Array(
-                Buffer.from(trustedSigner.privateKey.slice(2), "hex")
-            );
-            const signedProof = EncryptionUtils.signData(signedProofMsg, privateKeyArray)
+            const signedData = await trustedSignerNetworkInfo.keyring.sign(signedDataMsg)
+            
+            // Fetch the context proof from the signing DID
+            const signedProof = await trustedSignerDIDDocument.locateContextProof(CONTEXT_NAME)
 
             // Claim SBT
             const orgTotalSupply = parseInt(await blockchainApi.totalSupply())
 
             await blockchainApi.claimSBT(
                 sbtType,
-                sbtId,
+                uniqueId,
                 sbtURI,
-                claimer,
+                RECIPIENT_WALLET.address,
                 signedData,
-                signedProof                
+                signedProof!
             )
 
             const newTotalSupply = parseInt(await blockchainApi.totalSupply())
