e8137a4fca981c3b07e2bd3dd6a97442bc962d7f
Chris <chris@verida.io>
2023-06-27 12:44:01 +0930

    Support fetching a list of active DIDs from the blockchain

---
+++ packages/vda-did-resolver/src/getDIDs.ts
@@ -0,0 +1,37 @@
+import { CONTRACT_ADDRESS, CONTRACT_ABI as abiList, RPC_URLS } from "@verida/vda-common";
+
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Contract } from 'ethers';
+  
+/**
+ * Call lookUp() function of DIDRegistry contract
+ * @param network Verida network to retreieve DIDs for
+ * @param startIndex Index to start
+ * @param count Total number of results to fetch
+ */
+export async function getDIDs(network: string, startIndex: number = 0, count: number=20): Promise<string[]> {
+    const rpcUrl = RPC_URLS[network]
+    if (!rpcUrl) {
+        throw new Error(`Unable to locate RPC_URL for network: ${network}`)
+    }
+
+    // Simple read-only of the blockchain
+    const contractABI = abiList["VeridaDIDRegistry"];
+    const provider = new JsonRpcProvider(rpcUrl);
+    const address = CONTRACT_ADDRESS["VeridaDIDRegistry"][network];
+
+    if (!address) {
+        throw new Error(`Empty contract address for network-${network}`)
+    }
+
+    const contract = new Contract(address, contractABI.abi, provider);
+    
+    let data;
+    try {
+        data = await contract.callStatic.getDIDs(startIndex, count)
+    } catch (e: any) {
+        throw new Error('Failed to get list of active DIDs');
+    }
+
+    return data
+}
---
+++ packages/vda-did-resolver/test/getDIDs.test.ts
@@ -0,0 +1,17 @@
+const assert = require('assert')
+import { getDIDs } from '../src/getDIDs';
+
+const NETWORK = 'testnet'
+
+describe('getDIDs test', function() {
+    this.timeout(20000)
+    
+    it('Get first 20 created DIDs', async () => {
+        const result = await getDIDs(NETWORK, 0, 20);
+
+        assert.ok(result, 'Have a result')
+        assert.equal(typeof(result), 'array', 'Result is a number')
+        assert.ok(result.length, 'Have at least one result returned')
+        console.log(result)
+    });
+})
