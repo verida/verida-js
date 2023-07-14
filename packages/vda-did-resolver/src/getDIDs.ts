b7f0020c3cbfdacbe3754f829245906aa16df433
nick-verida <nick@verida.io>
2023-07-14 15:34:49 +0930

    map the DID string to the correct DID format

--- packages/vda-did-resolver/src/getDIDs.ts
+++ packages/vda-did-resolver/src/getDIDs.ts
@@ -1,4 +1,4 @@
-import { CONTRACT_ADDRESS, CONTRACT_ABI as abiList, RPC_URLS } from "@verida/vda-common";
+import { CONTRACT_ADDRESS, RPC_URLS, CONTRACT_ABI as abiList } from "@verida/vda-common";
 
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Contract } from 'ethers';
@@ -10,7 +10,7 @@ import { activeDIDCount } from "./activeDIDCount";
  * @param startIndex Index to start
  * @param count Total number of results to fetch
  */
-export async function getDIDs(network: string, startIndex: number = 0, count: number=20, mostRecent: boolean=true): Promise<string[]> {
+export async function getDIDs(network: string, startIndex: number=0, count: number=20, mostRecent: boolean=true): Promise<string[]> {
     const rpcUrl = RPC_URLS[network]
     if (!rpcUrl) {
         throw new Error(`Unable to locate RPC_URL for network: ${network}`)
@@ -28,13 +28,21 @@ export async function getDIDs(network: string, startIndex: number = 0, count: nu
     const contract = new Contract(address, contractABI.abi, provider);
 
     if (mostRecent) {
-        const activeDids = await activeDIDCount(network)
-        startIndex = activeDids - startIndex - count
+        // user wants most recent DIDs
+        // we need to calculate the correct offset
+        // Note that startIndex is indexed from the **end** of the list of DIDs
+        //   so a startIndex of 0 and count of 20 means get the 20 most recent.
+        const activeDidCount = await activeDIDCount(network)
+        startIndex = activeDidCount - startIndex - count
     }
     
-    let data;
+    let data: string[] | undefined = undefined;
     try {
-        data = await contract.callStatic.getDIDs(startIndex, count)
+        data = (await contract.callStatic.getDIDs(startIndex, count)) as string[];
+        data.map((did) => {
+            // map the raw string into the correct canonoical DID format
+            return `did:vda:${network}:${did}`;
+        })
     } catch (e: any) {
         throw new Error('Failed to get list of active DIDs');
     }
