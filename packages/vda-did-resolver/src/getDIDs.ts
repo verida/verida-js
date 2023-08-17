258300818cf0b554af8c3fbec84421901db64c30
nick-verida <87622993+nick-verida@users.noreply.github.com>
2023-08-17 17:21:02 +0930

    Closes 338. Map the DID string to the correct DID format (#339)
    Map the DID string to the correct DID format.

--- packages/vda-did-resolver/src/getDIDs.ts
+++ packages/vda-did-resolver/src/getDIDs.ts
@@ -1,8 +1,9 @@
-import { CONTRACT_ADDRESS, CONTRACT_ABI as abiList, RPC_URLS } from "@verida/vda-common";
+import { CONTRACT_ADDRESS, RPC_URLS, CONTRACT_ABI as abiList } from "@verida/vda-common";
 
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Contract } from 'ethers';
 import { activeDIDCount } from "./activeDIDCount";
+import { EnvironmentType } from '@verida/types'
   
 /**
  * Call lookUp() function of DIDRegistry contract
@@ -10,7 +11,7 @@ import { activeDIDCount } from "./activeDIDCount";
  * @param startIndex Index to start
  * @param count Total number of results to fetch
  */
-export async function getDIDs(network: string, startIndex: number = 0, count: number=20, mostRecent: boolean=true): Promise<string[]> {
+export async function getDIDs(network: EnvironmentType, startIndex: number=0, count: number=20, mostRecent: boolean=true): Promise<string[]> {
     const rpcUrl = RPC_URLS[network]
     if (!rpcUrl) {
         throw new Error(`Unable to locate RPC_URL for network: ${network}`)
@@ -25,19 +26,25 @@ export async function getDIDs(network: string, startIndex: number = 0, count: nu
         throw new Error(`Empty contract address for network-${network}`)
     }
 
-    const contract = new Contract(address, contractABI.abi, provider);
-
-    if (mostRecent) {
-        const activeDids = await activeDIDCount(network)
-        startIndex = activeDids - startIndex - count
-    }
-    
-    let data;
     try {
-        data = await contract.callStatic.getDIDs(startIndex, count)
+        const contract = new Contract(address, contractABI.abi, provider);
+
+        if (mostRecent) {
+            // user wants most recent DIDs
+            // we need to calculate the correct offset
+            // Note that startIndex is indexed from the **end** of the list of DIDs
+            //   so a startIndex of 0 and count of 20 means get the 20 most recent.
+            const activeDidCount = await activeDIDCount(network)
+            startIndex = activeDidCount - startIndex - count
+        }
+        
+        const data = (await contract.callStatic.getDIDs(startIndex, count)) as string[];
+
+        // map the raw string into the correct canonoical DID format
+        return (data || []).map((did) => `did:vda:${network}:${did}`)
+
     } catch (e: any) {
         throw new Error('Failed to get list of active DIDs');
     }
 
-    return data
 }
