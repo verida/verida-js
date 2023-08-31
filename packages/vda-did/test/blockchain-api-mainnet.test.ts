3201343e3292bf531e617d5ad5c32ccb20c1f7c7
Alex <trust2ray@gmail.com>
2023-08-31 16:35:10 +0900

    [vda-did] : Added test file for mainnet transactions
    - Updated the .env.example to support mainnet and testnet
    - Added /test/blockchain-api-mainnet.test.ts

---
+++ packages/vda-did/test/blockchain-api-mainnet.test.ts
@@ -0,0 +1,71 @@
+const assert = require('assert')
+import { getBlockchainAPIConfiguration, DID_LIST } from "@verida/vda-common-test";
+import BlockchainApi from "../src/blockchain/blockchainApi"
+import { Wallet } from "ethers";
+require('dotenv').config();
+
+// const did = Wallet.createRandom();
+const did = new Wallet(DID_LIST[0].privateKey);
+
+const endPoints_A = ['https://A_1', 'https://A_2', 'https://A_3'];
+const endPoints_B = ['https://B_1', 'https://B_2'];
+const endPoints_Empty: string[] = [];
+
+const privateKey = process.env.PRIVATE_KEY;
+if (!privateKey) {
+    throw new Error('No PRIVATE_KEY in the env file');
+}
+
+const configuration = getBlockchainAPIConfiguration(privateKey);
+const createBlockchainAPI = (did: any) => {
+    return new BlockchainApi({
+        identifier: did.address,
+        signKey: did.privateKey,
+        chainNameOrId: "mainnet",
+        ...configuration
+    })
+}
+
+describe('vda-did blockchain api', () => {
+    let blockchainApi : BlockchainApi
+    before(() => {
+        blockchainApi = createBlockchainAPI(did);
+    })
+
+    describe('register', function() {
+        this.timeout(100 * 1000)
+
+        it('Register successfully', async () => {
+            await blockchainApi.register(endPoints_A);
+
+            const lookupResult = await blockchainApi.lookup(did.address);
+            assert.deepEqual(
+                lookupResult, 
+                {didController: did.address, endpoints: endPoints_A},
+                'Get same endpoints');
+        })
+
+        it('Should update for registered DID', async () => {
+            await blockchainApi.register(endPoints_B);
+
+            const lookupResult = await blockchainApi.lookup(did.address);
+            assert.deepEqual(
+                lookupResult, 
+                {didController: did.address, endpoints: endPoints_B}, 
+                'Get updated endpoints');
+        })
+
+        it('Should reject for revoked did', async () => {
+            const tempDID = Wallet.createRandom();
+            const testAPI = createBlockchainAPI(tempDID)
+
+            await testAPI.register(endPoints_Empty);
+            await testAPI.revoke();
+
+            await assert.rejects(
+                testAPI.register(endPoints_A),
+                {message: 'Failed to register endpoints'}
+            )
+        })
+    })
+})
