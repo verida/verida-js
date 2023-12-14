09b91a2a71f5fbbb5c6d0ae608b0bb7fd2a5d96b
Alex <trust2ray@gmail.com>
2023-12-14 10:46:33 +0900

    [types] V2.5.0 Added types for `StorageNode`

---
+++ packages/types/src/IStorageNode.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Status of StoargeNode and DataCenter
+ * Used in `vda-node-manager` and `vda-node-client` packages
+ */
+export enum EnumStatus {
+  removed = 0,
+  removing,
+  active
+}
