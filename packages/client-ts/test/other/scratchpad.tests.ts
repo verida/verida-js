ae48d2bff7324a84b8710459252d97938991f925
Chris <chris@verida.io>
2023-12-30 09:26:40 +1030

    Add more scratchpad utilities

--- packages/client-ts/test/other/scratchpad.tests.ts
+++ packages/client-ts/test/other/scratchpad.tests.ts
@@ -6,8 +6,8 @@ import CONFIG from '../config'
 import { Wallet } from 'ethers'
 import { EnvironmentType } from '@verida/types'
 
-const ENVIRONMENT = EnvironmentType.TESTNET
-const CONTEXT_NAME = ''
+const ENVIRONMENT = EnvironmentType.MAINNET
+const CONTEXT_NAME = 'Verida: Vault'
 const DB_NAME = ''
 
 const seedPhrase = ``
@@ -16,7 +16,7 @@ const privateKey = Wallet.fromMnemonic(seedPhrase).privateKey
 /**
  * 
  */
-describe.skip('Verida basic database tests', () => {
+describe('Verida basic database tests', () => {
     let context, did, account
 
     const network = new Client({
@@ -26,10 +26,10 @@ describe.skip('Verida basic database tests', () => {
         }
     })
 
-    describe('Basic tests', function() {
+    describe('Open network connection', function() {
         this.timeout(600000)
         
-        it('can do stuff', async function() {
+        it('can connect and load DID', async function() {
             // Initialize account 1
             account = new AutoAccount({
                 privateKey,
@@ -39,18 +39,33 @@ describe.skip('Verida basic database tests', () => {
             did = await account.did()
             console.log(did)
             await network.connect(account)
+        })
+
+        it('can load DID doc', async function() {
+            const didDoc = (await network.didClient.get(did)).export()
+            console.log(didDoc)
+            console.log(didDoc.service)
+        })
+
+        it('can output all contexts', async function() {
+        })
+
+        it('can output all databases for context', async function() {
             context = await network.openContext(CONTEXT_NAME, false)
 
-            const database = await context.openDatabase(DB_NAME)
+            const database = await context.openDatabase('storage_database')
             const data = await database.getMany()
-
-            console.log(data)
+            for (let i in data) {
+                console.log(data[i].dbName, data[i].dbHash, data[i].endpoint)
+            }
         })
 
         this.afterAll(async () => {
-            await context.close({
-                clearLocal: true
-            })
+            if (context) {
+                await context.close({
+                    clearLocal: true
+                })
+            }
         })
     })
 })
