f4b791e4ca5a5c3e89842e7836e929030451a5a9
Alex <trust2ray@gmail.com>
2024-03-25 10:48:15 +0900

    [vda-common] v3.0.2
    - Updated the `StorageNodeRegistry` contract address

--- packages/vda-common/src/contract.ts
+++ packages/vda-common/src/contract.ts
@@ -63,9 +63,9 @@ export const CONTRACT_ADDRESS : Record<CONTRACT_NAMES, Record<string, string | n
     "StorageNodeRegistry": {
         mainnet: null,
         "0x89": null,
-        testnet: "0x8b5243DF296E386c8ff58bbb39DF97F431C10dd6",
-        "0x13881": "0x8b5243DF296E386c8ff58bbb39DF97F431C10dd6",
-        devnet: "0x8b5243DF296E386c8ff58bbb39DF97F431C10dd6"
+        testnet: "0xDfe326357A4078A3ea8A192084255F25Ab73c712",
+        "0x13881": "0xDfe326357A4078A3ea8A192084255F25Ab73c712",
+        devnet: "0xDfe326357A4078A3ea8A192084255F25Ab73c712"
     }
 };
 
