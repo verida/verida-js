db2d8698917fef26c6385e7c01846461b661226c
Chris <chris@verida.io>
2025-04-15 20:56:06 +0930

    Update Polygon mainnet RPC to latest

--- packages/vda-common/src/rpc.ts
+++ packages/vda-common/src/rpc.ts
@@ -1,13 +1,12 @@
 /* Default RPC URLs to the Polygon blockchains. These default endpoints are public and free but potentially unstable */
 export const RPC_URLS: Record<string, string | null> = {
-    // Ankr is the official RPC provider of the Polygon foundation
-    // https://rpc.ankr.com/polygon is the same as https://polygon-rpc.com as also provided by Ankr
-    mainnet: "https://rpc.ankr.com/polygon",
-    "0x89": "https://rpc.ankr.com/polygon",
+    // https://polygon-rpc.com is the official RPC provider of the Polygon foundation
+    mainnet: "https://polygon-rpc.com",
+    "0x89": "https://polygon-rpc.com",
     testnet: "https://rpc-amoy.polygon.technology",
     "0x13882": "https://rpc-amoy.polygon.technology",
     devnet: "https://rpc-amoy.polygon.technology",
-    polpos: "https://rpc.ankr.com/polygon",
+    polpos: "https://polygon-rpc.com",
     polamoy: "https://rpc-amoy.polygon.technology"
 };
 
